7110f88a6b802a91007eb2b6578c4d1d7558a4f1
Torsten Sommer <torsten.sommer@3ds.com>
2017-12-11 22:11:46 +0100

    Bump version to 0.1.1. Add dask and matplotlib as requirements.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-WHL_FILE=https://pypi.python.org/packages/5b/10/a055c4dbe7e63336af68840b71452dd09224f8b6e2ed3d5a44f181b146e1/FMPy-${PKG_VERSION}-py2.py3-none-any.whl
+WHL_FILE=https://pypi.python.org/packages/b9/74/3a6e6939e6d4058361837effbac8c1b5140aaba579dcf59ced267eb057f5/FMPy-${PKG_VERSION}-py2.py3-none-any.whl
 
 pip install --no-deps ${WHL_FILE}
