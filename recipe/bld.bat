f903f021a470f2d3d1fd18719684f462e885def3
Torsten Sommer <torsten.sommer@3ds.com>
2017-12-21 09:17:31 +0100

    Bump version to 0.1.2. Simplify build command.

--- recipe/bld.bat
+++
@@ -1,1 +0,0 @@
-%PYTHON% -m pip install --no-deps https://pypi.python.org/packages/b9/74/3a6e6939e6d4058361837effbac8c1b5140aaba579dcf59ced267eb057f5/FMPy-%PKG_VERSION%-py2.py3-none-any.whl
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WHL_FILE=https://pypi.python.org/packages/b9/74/3a6e6939e6d4058361837effbac8c1b5140aaba579dcf59ced267eb057f5/FMPy-${PKG_VERSION}-py2.py3-none-any.whl
-
-pip install --no-deps ${WHL_FILE}
